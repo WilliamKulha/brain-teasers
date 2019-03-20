40c21be9ece13dbb0f7cf9dc0d5e520a673132af
WilliamKulha <wkulha14@gmail.com>
2019-03-20 07:40:07 -0400

    Remove a bunch of stuff that just wouldn't work. Comment on top where it should go from here.

--- stringtoequation.js
+++ stringtoequation.js
@@ -1,3 +1,7 @@
+//Very hard to make this work if it contains multiplication and division.
+//If just + and - then it's okay. looop through calling operators consecutively and you'll get to the end.
+//If you're allowed to use anything at your disposal, use eval(string);
+
 let string = '1+3-2*7';
 let eq = string.split('');
 
@@ -6,8 +10,4 @@ let operators = {
   '-': (num1, num2) => { parseInt(num1) - parseInt(num2);},
   '*': (num1, num2) => {parseInt(num1) * parseInt(num2);},
   '/': (num1, num2) => {parseInt(num1) / parseInt(num2);}
-}
-
-if (eq.includes('*') || eq.includes('/')) {
-  let 
 }
